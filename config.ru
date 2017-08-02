cb048183e6ef2e9fa491a042dd96c32dd06140e6
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-08-02 03:12:02 +0000

    refactor

--- config.ru
+++ config.ru
@@ -1,14 +1,9 @@
-use Rack::Static
+require 'rack'
+require './nancy'
+
 # from http://rubylearning.com/blog/2013/04/02/whats-rack/
 # https://devcenter.heroku.com/articles/static-sites-ruby
+# https://robots.thoughtbot.com/lets-build-a-sinatra
 
-run lambda { |env|
-  [
-    200,
-    {},
-    ["Hello. The time is #{Time.now}"]
-  ]
-}
+run Nancy::Base.nancy
 
-
-# use: rackup -p $PORT -o $IP
--- nancy.rb
+++ nancy.rb
@@ -1,41 +1,50 @@
-# nancy.rb
-require "rack"
-
 module Nancy
-  class Base
-    def initialize
-      @routes = {}
+    class Base
+        @@nancy = nil
+        
+        def initialize
+            @routes = {}
+        end
+
+        attr_reader :routes
+
+        def get(path, &handler)
+            route("GET", path, &handler)
+        end
+    
+        def call(env)
+            @request = Rack::Request.new(env)
+            verb = @request.request_method
+            requested_path = @request.path_info
+        
+            handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
+            if handler
+                handler.call
+            else
+                [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
+            end
+        end
+    
+        def self.initialize
+            @@nancy = self.new
+            @@nancy.get "/hello" do
+                [200, {}, ["Nancy says hello"]]
+            end
+        end
+    
+        def self.nancy
+            if @@nancy
+                return @@nancy
+            end
+            self.initialize
+            @@nancy
+        end
+
+        private
+
+        def route(verb, path, &handler)
+            @routes[verb] ||= {}
+            @routes[verb][path] = handler
+        end
     end
-
-    attr_reader :routes
-
-    def get(path, &handler)
-      route("GET", path, &handler)
-    end
-
-    private
-
-    def route(verb, path, &handler)
-      @routes[verb] ||= {}
-      @routes[verb][path] = handler
-    end
-  end
 end
-
-def call(env)
-  @request = Rack::Request.new(env)
-  verb = @request.request_method
-  requested_path = @request.path_info
-
-  handler = @routes[verb][requested_path]
-
-  handler.call
-end
-
-nancy = Nancy::Base.new
-
-nancy.get "/hello" do
-  [200, {}, ["Nancy says hello"]]
-end
-
-puts nancy.routes
